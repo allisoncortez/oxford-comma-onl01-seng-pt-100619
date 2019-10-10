4c977ffb3b6eb662b2383e850a9ae9426178a0cd
allison cortez <allizetorc@gmail.com>
2019-10-10 00:56:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,7 +14,7 @@ end
 #   elsif array.size == 2
 #     return array.join(" and ")
 #   else
-#     return array[0..-2].join(', ') + ", and " + array[-1]
+#     return array[0..-2].join(', ') + ", and " + array[ -1 ]
 #   end
 # end
 
