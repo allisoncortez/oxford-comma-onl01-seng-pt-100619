4e988f171aac183cc60a9105b7392ca34a9d4c7b
allison cortez <allizetorc@gmail.com>
2019-10-10 01:01:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array[0]
   elsif array.size == 2
     return array.join(" and ")
-  elsif array.size >= 3
+  elsif array.size == 3
     return array.join(", ") + ", and " + array[-1]
   end
 end
