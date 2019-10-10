e5bbfabc5a540384d6709f2a8208640f859fa06c
allison cortez <allizetorc@gmail.com>
2019-10-10 01:05:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,25 +7,3 @@ def oxford_comma(array)
     return array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
-
-# def oxford_comma(array)
-#   if array.size == 1
-#     return array[0]
-#   elsif array.size == 2
-#     return array.join(" and ")
-#   else
-#     return array[0..-2].join(', ') + ", and " + array[ -1]
-#   end
-# end
-
-
-# def oxford_comma(array)
-#   if array.length == 1
-#     return "#{array[0]}"
-#   elsif array.length == 2
-#     return array.join(" and ")
-#   elsif array.length >= 3
-#     array[-1] = "and #{array[-1]}"
-#     return array.join(", ")
-#   end
-# end
